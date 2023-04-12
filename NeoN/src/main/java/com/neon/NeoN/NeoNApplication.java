fd6c85f8a0796b3367c197503a1c61e8aba9a8cf
My PC <alex.petkov606@gmail.com>
2023-04-12 13:15:08 +0300

    Initial commit

---
+++ NeoN/src/main/java/com/neon/NeoN/NeoNApplication.java
@@ -0,0 +1,13 @@
+package com.neon.NeoN;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NeoNApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NeoNApplication.class, args);
+	}
+
+}
---
+++ NeoN/src/test/java/com/neon/NeoN/NeoNApplicationTests.java
@@ -0,0 +1,13 @@
+package com.neon.NeoN;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NeoNApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
