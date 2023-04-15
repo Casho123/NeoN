59ea73eb2d660813053b27ad8499b197cc23607c
My PC <alex.petkov606@gmail.com>
2023-04-15 15:54:02 +0300

    asd

--- NeoN/src/main/java/com/neon/NeoN/controller/UserRegistrationController.java
+++ NeoN/src/main/java/com/neon/NeoN/controller/UserRegistrationController.java
@@ -1,15 +1,45 @@
 package com.neon.NeoN.controller;
 
 
+import com.neon.NeoN.models.dto.UserRegistrationDTO;
+import com.neon.NeoN.service.UserService;
 import org.springframework.stereotype.Controller;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
+
+import javax.validation.Valid;
 
 @Controller
 public class UserRegistrationController {
 
+    private final UserService userService;
+
+    public UserRegistrationController(UserService userService) {
+        this.userService = userService;
+    }
+
+
+    @ModelAttribute("userRegistrationDTO")
+    public UserRegistrationDTO initUser() {
+        return new UserRegistrationDTO();
+    }
+
     @GetMapping("/register")
     public String register() {
-
         return "register";
     }
+
+    @PostMapping("/register")
+    public String register(@Valid UserRegistrationDTO userRegistrationDTO,
+                           BindingResult bindingResult,
+                           RedirectAttributes redirectAttributes) {
+
+
+        return "redirect:/login";
+    }
+
+
 }
