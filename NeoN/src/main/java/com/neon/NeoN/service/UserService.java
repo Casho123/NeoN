41a6ddfbef3dce9d1c9981cd1c79fd6ae61d745d
My PC <alex.petkov606@gmail.com>
2023-04-15 15:51:13 +0300

    asd

---
+++ NeoN/src/main/java/com/neon/NeoN/service/UserService.java
@@ -0,0 +1,21 @@
+package com.neon.NeoN.service;
+
+
+import com.neon.NeoN.models.dto.UserRegistrationDTO;
+import com.neon.NeoN.repository.UserRepository;
+import org.springframework.stereotype.Service;
+
+@Service
+public class UserService {
+
+    private final UserRepository userRepository;
+
+    public UserService(UserRepository userRepository) {
+        this.userRepository = userRepository;
+    }
+
+
+    public void register(UserRegistrationDTO userRegistrationDTO) {
+        //TODO
+    }
+}
